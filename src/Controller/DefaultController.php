7ec2eea6af847838d0ec9ed7a4f558797ebbc755
Julien <steeper@live.fr>
2018-11-08 22:57:56 +0100

    BELLA WOUHOUHOUUUU BELLA

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -36,38 +36,29 @@ class DefaultController extends AbstractController
     public function index()
     {
         $magasinsManager = new MagasinsManager($this->getPdo());
-        $magasins = $magasinsManager->selectAll();
-        $magasinsReplace = $this->replace($magasins);
-        ?><pre><?php var_dump($magasinsReplace); ?></pre> <?php
-        die();
+        $magasins = $magasinsManager->selectAll(); // je select tout les magasins
+        $magasinsReplace = $this->replace($magasins); // j'envoie tout les magasins dans ma fonction
 
         return $this->twig->render('index.html.twig', ['magasins' => $magasinsReplace]); // retourne la vue
     }
 
-    public function replace($magasins)
+    public function replace($allMagasins)
     {
-        foreach ($magasins as $magasin)
+        foreach ($allMagasins as $magasin) // je boucle sur chaque objet de mon tableau ( magasin )
         {
-            $nameMagasin = "";
-            $replace = $magasin->getContent();
+            $newContentMagasin = ""; // j'instancie à chaque fois une nouvelle string vide
+            $replace = $magasin->getContent(); // je récupère le contenu de mon magasin
 
-            $name = explode(" ", $replace);
+            $explode = explode(" ", $replace); // je divise la string en plusieurs variable dès qu'il y a un espace ( toutes ces variables sont socké dans un tableau )
 
-            foreach ($name as $magasinName)
+            foreach ($explode as $variableExplode) // je boucle sur le tableau explode
             {
-                str_replace(" ", "", $magasinName);
-                $nameMagasin .= $magasinName . " ";
+                if(!empty($variableExplode)) // si l'élément de mon tableau n'est pas vide alors
+                    $newContentMagasin .= $variableExplode . " "; // j'ajoute cet élément avec un espace à ma string instancié au dessus
             }
-            $magasin->setContent($nameMagasin);
+            $magasin->setContent($newContentMagasin); // puis j'envoie cette string en tant que nouveau contenu de mon magasin
         }
-        return $magasins;
+        return $allMagasins;
     }
 
 }
-
-/*        $userManager = new UserManager($this->getPdo()); // new object
-        $users = $userManager->selectAll(); // sélectionne tous les éléments
-        foreach ($users as $user) // pour chaque élément on boucle dessus
-            $user->getFirstname //get de la valeur désirée
-                explode // balance dans la fonction remplacante de ltrim
-            $user->setFirstname */
